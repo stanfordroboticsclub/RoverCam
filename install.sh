4f0183dd693f8578a84814bfa86f24d0f7148b85
Julia Thompson <julia.thompson@stanford.edu>
2018-12-08 18:08:26 -0800

    haaaaands

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+FOLDER=$(dirname $(realpath "$0))
+cd $FOLDER
+
+git clone https://github.com/131/h264-live-player.git player
+cd player
+yes | npm install
+
+for file in *.service; do
+	[ -f "$file"] || break
+	sudo ln -s $FOLDER/$file /lib/systemd/system
+done
