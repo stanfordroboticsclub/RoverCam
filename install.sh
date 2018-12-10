044165924d99c05c1b2b3d254b8c69d61c058866
mikh3x4 <mikh3x4@users.noreply.github.com>
2018-12-10 03:07:40 -0800

    returns to main directory before copying services

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ git clone https://github.com/131/h264-live-player.git player
 cd player
 yes | npm install
 
+cd $FOLDER
+
 for file in *.service; do
     [ -f "$file" ] || break
     sudo ln -s $FOLDER/$file /lib/systemd/system
