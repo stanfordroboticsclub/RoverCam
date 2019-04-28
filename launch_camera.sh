940ebc68c2d7b6311688481fe2df2ea6e6fa0c2b
Neil Movva <neilmovva97@gmail.com>
2019-04-28 15:22:28 -0700

    support mDNS names instead of hard ips

--- launch_camera.sh
+++ launch_camera.sh
@@ -1,14 +1,16 @@
 args=("$@")
 
 if [ "$#" -ne 1 ]; then
-    echo "Need exactly one parameter: camera ID number"
+    echo "Need exactly one parameter: camera mDNS name"
     exit 2
 fi
 
 
 rgx_ip="inet (10\.0\.0\.[0-9]+)"
-port="500${args[0]}"
-pi_ip="10.0.0.2${args[0]}"
+rgx_ip_any="([0-9]+\.[0-9]+\.[0-9]+\.([0-9]+))"
+
+# port="500${args[0]}"
+# ip_pi="10.0.0.2${args[0]}"
 
 cam_launch_hi="gst-launch-1.0 rpicamsrc preview=false bitrate=2000000 sensor-mode=5 \
 ! 'video/x-h264,width=1280,height=720,framerate=45/1,profile=high' "
@@ -31,18 +33,30 @@ cmd_ip=$(ifconfig)
 
 if [[ $cmd_ip =~ $rgx_ip ]]
     then
-        ip="${BASH_REMATCH[1]}"
-        echo "detected viewer machine IP address: ${ip}"    # concatenate strings
+        ip_viewer="${BASH_REMATCH[1]}"
+        echo "detected viewer machine IP address: ${ip_viewer}"    # concatenate strings
     else
         echo "failed to retrieve viewer IP address"
         exit 1
 fi
 
+cmd_resolve=$(avahi-resolve --name ${args[0]})
+if [[ $cmd_resolve =~ $rgx_ip_any ]]
+    then
+        ip_pi="${BASH_REMATCH[1]}"
+        port="${BASH_REMATCH[2]}"
+        echo "detected pi camera IP address: ${ip_pi}"    # concatenate strings
+    else
+        echo "failed to resolve pi's IP address"
+        exit 1
+fi
+
+
 gst-launch-1.0 udpsrc port=${port} ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false &
 
 
-cam_launch_suffix=" ! h264parse ! queue ! rtph264pay pt=96 ! gdppay ! udpsink host=${ip} port=$port"
+cam_launch_suffix=" ! h264parse ! queue ! rtph264pay pt=96 ! gdppay ! udpsink host=${ip_viewer} port=$port"
 
 cam_launch="${cam_launch_med}${cam_launch_suffix}"
 
-ssh -t pi@${pi_ip} ${cam_launch}
+ssh -t pi@${ip_pi} ${cam_launch}
