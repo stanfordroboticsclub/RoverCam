2001bab190f989789f28bd4eb333180dab9ab0c6
Neil Movva <neilmovva97@gmail.com>
2019-05-01 01:40:06 -0700

    adding install script (untested) removing legacy system

--- install.sh
+++ install.sh
@@ -3,13 +3,20 @@
 FOLDER=$(dirname $(realpath "$0"))
 cd $FOLDER
 
-yes | sudo apt-get install nodejs
+. raspi-config nonint
+do_camera 1
 
-git clone https://github.com/131/h264-live-player.git player
-cd player
-yes | npm install
 
-cd $FOLDER
+yes | sudo apt-get install \
+autoconf automake libtool pkg-config gstreamer1.0-tools gstreamer1.0-plugins-bad gstreamer1.0-plugins-good libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+
+wget https://github.com/thaytan/gst-rpicamsrc/archive/master.zip
+unzip master.zip
+cd gst-rpicamsrc-master
+./autogen.sh --prefix=/usr --libdir=/usr/lib/arm-linux-gnueabihf/
+make
+sudo make install
+
 
 for file in *.service; do
     [ -f "$file" ] || break
