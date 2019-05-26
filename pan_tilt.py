a36bda13bfef31771aee472f5855ace5d72aa055
Michal <mikh3x4@gmail.com>
2019-05-26 13:46:11 -0700

    impoved command of ptz

--- pan_tilt.py
+++ pan_tilt.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import time
-from UDPComms import Subscriber
+from UDPComms import Subscriber, timeout
 
-pan_pin = 2
-tilt_pin = 3
+pan_pin = 14
+tilt_pin = 4
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pan_pin, GPIO.OUT)
@@ -15,9 +15,12 @@ tilt = GPIO.PWM(tilt_pin, 50)
 pan.start(7.5)
 tilt.start(7.5)
 
-sub = Subscriber(9999, timeout=1)
+sub = Subscriber(8120, timeout=1)
 
 time.sleep(1)
+
+pan_angle = 0
+tilt_angle = 0
 
 def clamp(x,ma,mi):
     if x > ma:
@@ -29,12 +32,20 @@ def clamp(x,ma,mi):
 
 while True:
     time.sleep(0.1)
-    msg = sub.get()
+    changed = False
+    try:
+        msg = sub.get()
+        pan_angle = clamp(pan_angle + msg['pan'], 90, -90)
+        tilt_angle = clamp(tilt_angle + msg['tilt'], 90, -24)
+        if (msg['pan'] != 0) or (msg['tilt'] != 0):
+            changed = True
+    except timeout:
+        pass
 
-    pan_angle = clamp(msg['pan'], 90, -90)
-    tilt_angle = clamp(msg['tilt'], 90, -24)
+    if changed:
+        pan.ChangeDutyCycle(7.5 + pan_angle*5.5/90)
+        tilt.ChangeDutyCycle(8.5 - tilt_angle*5.5/90)
+    else:
+        pan.ChangeDutyCycle(0)
+        tilt.ChangeDutyCycle(0)
 
-    pan.ChangeDutyCycle(7.5 + pan_angle*5.5/90)
-    tilt.ChangeDutyCycle(8.5 - tilt_angle*5.5/90)
-
-    pass
