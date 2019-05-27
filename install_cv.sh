088bf50c115d246c7775f2e4c554e8755811abbc
Michal <mikh3x4@gmail.com>
2019-05-27 22:31:42 +0000

    install opencv script

---
+++ install_cv.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y libhdf5-dev libhdf5-serial-dev
+sudo apt-get install -y libqtwebkit4 libqt4-test
+sudo apt-get install -y libatlas-base-dev libjasper-dev libqtgui4 python3-pyqt5
+
+yes | sudo pip3 install opencv-python
