a3b88ea402151c626fdb3d4c3a6aeefab446e25a
Michal Adamkiewicz <mikh3x4@gmail.com>
2020-02-02 14:59:10 -0800

    usb with h264 on pi

--- opencv_remote.py
+++ opencv_remote.py
@@ -80,6 +80,9 @@ class Server:
               " ! h264parse ! rtph264pay pt=96 ! udpsink host={} port={}".format(host,port))
         # args = shlex.split(arg)
 
+        # works 180ms of laterncy
+        #gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480 ! x264enc bitrate=1000000 speed-preset=1 tune=zerolatency ! rtph264pay pt=96 ! udpsink host=10.0.0.54 port=5001
+
         self.process = subprocess.Popen(arg, shell=True)
         self.process.wait()
         print("video process died")
