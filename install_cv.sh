b3747c5682569fc30f8d8e37c6c27e8f0e53f186
Michal <mikh3x4@gmail.com>
2020-02-14 01:22:57 +0000

    added imultis and contrib to instal

--- install_cv.sh
+++ install_cv.sh
@@ -2,4 +2,7 @@ sudo apt-get install -y libhdf5-dev libhdf5-serial-dev
 sudo apt-get install -y libqtwebkit4 libqt4-test
 sudo apt-get install -y libatlas-base-dev libjasper-dev libqtgui4 python3-pyqt5
 
-yes | sudo pip3 install opencv-python
+# second has more modules
+# yes | sudo pip3 install opencv-python
+yes | sudo pip3 install opencv-contrib-python
+yes | sudo pip3 install imutils
