169879c6d02634079ab3c89ac1fb7fa9e3d6a812
Michal Adamkiewicz <mikh3x4@gmail.com>
2020-03-05 01:17:34 -0800

    force opecv version

--- install_cv.sh
+++ install_cv.sh
@@ -2,7 +2,10 @@ sudo apt-get install -y libhdf5-dev libhdf5-serial-dev
 sudo apt-get install -y libqtwebkit4 libqt4-test
 sudo apt-get install -y libatlas-base-dev libjasper-dev libqtgui4 python3-pyqt5
 
+#might also need libavformat58 libswscale5
+
 # second has more modules
 # yes | sudo pip3 install opencv-python
-yes | sudo pip3 install opencv-contrib-python
+# yes | sudo pip3 install opencv-contrib-python #needs to force version as newest version has issues
+yes | pip install opencv-contrib-python==4.1.0.25
 yes | sudo pip3 install imutils
